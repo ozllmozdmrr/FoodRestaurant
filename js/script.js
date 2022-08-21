07aa0f114eaa9ac8eeebfa800578f0a31e0a7786
Özlem Özdemir <ozllmozdmrr00@gmail.com>
2022-08-21 16:04:43 +0300

    Js file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let searchForm = document.querySelector('.search-form-container');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
