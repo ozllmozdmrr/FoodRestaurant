45dfa674e21b7377bc25ab93db2daa8bebb6b57d
Özlem Özdemir <ozllmozdmrr00@gmail.com>
2022-08-22 22:29:05 +0300

    feat : screen responsive update

--- js/script.js
+++ js/script.js
@@ -41,3 +41,17 @@ document.querySelector('#menu-btn').onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+document.querySelector('.home').onmousemove = (e) => {
+
+    let x = (window.innerWidth - e.pageX * 2) /90;
+    let y = (window.innerHeight - e.pageY * 2) /90;
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px) translateY(${x}px)`;
+}
+
+document.querySelector('.home').onmouseleave = () => {
+
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
+}
